292638c95e682f8b39b31421a6ba1d1283d07db0
Devon Kooker <42676121+NitroTree48@users.noreply.github.com>
2022-05-03 00:18:22 -0500

    Changed original upload

--- FTT_Analyzer.py
+++ FTT_Analyzer.py
@@ -3,6 +3,8 @@ import pyaudio as pa
 import struct 
 import matplotlib.pyplot as plt
 import math
+import time
+import socket
 
 BUFFER = 1500
 FORMAT = pa.paInt16
@@ -20,31 +22,40 @@ stream = p.open(
     frames_per_buffer = BUFFER
 )
 
-# FFT
-
-#FFT line change
+# FFT Initial Setup
 data = stream.read(BUFFER)
 dataInt = struct.unpack(str(BUFFER) + 'h', data)
 fft_data = np.abs(np.fft.fft(dataInt))*2/(11000*BUFFER)
 
+# number of seconds per dance move
+dance_rate = 2.0
+time_since_last_dance = time.time()
 
-# FTT Frequency tracking
-# freqs = np.fft.fftfreq(len(np.fft.fft(dataInt)))
-# for idx, freq in enumerate(freqs[:len(freqs)]):
-#     print("Freq:", int(abs(freq * RATE)), end='' )
-#     # print(" Pwer:", fft_data[0])
-#     print("\n")
-    
-# print("Freq:", int(abs(freqs[len(freqs)//165] * RATE)))
+hml_avg = {'H': 0, 'M': 0, 'L': 0}
+last_move = True
 
 while True:
+
+    #send average frequency every time interval
+    if time.time() - time_since_last_dance > dance_rate:
+        max = ('L', 0)
+        hml_avg['H'] = round(hml_avg['H']*1.7, 3)
+        hml_avg['M'] = round(hml_avg['M'], 3)
+        hml_avg['L'] = round(hml_avg['L'] *.35, 3)
+        for x in hml_avg.items():
+            max = x if x[1] > max[1] else max
+        print(hml_avg)
+        print(max[0])
+        time_since_last_dance = time.time()
+        last_move = not last_move
+        hml_avg = {'H': 0, 'M': 0, 'L': 0}
+
     # Audio Data Input 
     data = stream.read(BUFFER)
     dataInt = struct.unpack(str(BUFFER) + 'h', data)
     fft_data = np.abs(np.fft.fft(dataInt))*2/(11000*BUFFER)
-   
 
-    # FFT Plotting
+    # Frequecy Ranges
     
     highFreqs = round(np.mean( fft_data[(len(fft_data)//35):(len(fft_data)-1)//2] ),3) 
 
@@ -52,17 +63,12 @@ while True:
 
     lowFreqs = round(np.mean( fft_data[0:(len(fft_data)//185)] ),3) 
 
-    if highFreqs >= 0.002 and highFreqs != 0.0:
-        print("High: ", highFreqs)
+    if highFreqs >= 0.005 and highFreqs != 0.0:
+        hml_avg['H'] += round(highFreqs,3)
 
-    if midFreqs >= 0.02 and midFreqs != 0.0:
-        print("Mid: ", midFreqs)
+    if midFreqs >= 0.06 and midFreqs != 0.0:
+        hml_avg['M'] += round(midFreqs, 3)
         
-    if lowFreqs >= 0.1 and lowFreqs != 0.0: 
-        print("Low: ", lowFreqs)
-
-
-    # specific frequency testing
-
-    # if round(fft_data[1], 3) > 0.0 and round(fft_data[1], 3) != 0.0: 
-    #    print("freq: ", int(abs(freqs[1] * RATE))," Power:", round(fft_data[1], 3))
+    if lowFreqs >= 0.3 and lowFreqs != 0.0: 
+        hml_avg['L'] += round(lowFreqs, 3)
+        
